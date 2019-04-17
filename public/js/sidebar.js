9b2e362185511e9b1f4bfefb7e59fc729c31f8f2
Jason <duan_jason@outlook.com>
2019-04-17 16:05:12 -0400

    add filter box on menu page and fix bugs

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,13 +1,37 @@
-$(document).ready(function () {
-    const dishInfo = $('.dish-info-wrap');
-    const food_names = dishInfo.find('h6').text().toLowerCase();
-    const recipes = dishInfo.find('ul li p').text().toLowerCase();
-    const totalText = food_names + " " + recipes;
+const hideOrShowMenuItem = (inKey, exKey) => {
+    $(".slider-menu .featured-responsive").filter(function () {
+            const menuItemName = $(this).find('h6').text().toLowerCase();
+            const menuItemRecipe = $(this).find('ul li p').text().toLowerCase();
 
+            $(this).show();
+            if (!exKey) {
+                if (inKey && menuItemName.indexOf(inKey) < 0
+                    && menuItemRecipe.indexOf(inKey) < 0) {
+                    $(this).hide();
+                }
+            } else {
+                // hide the menu item which user doesn't like
+                if (menuItemName.indexOf(exKey) > -1 || menuItemRecipe.indexOf(exKey) > -1) {
+                    $(this).hide();
+                } else {
+                    // display the menu item which user likes
+                    if (inKey && menuItemName.indexOf(inKey) < 0
+                        && menuItemRecipe.indexOf(inKey) < 0) {
+                        $(this).hide();
+                    }
+                }
+            }
+        }
+    )
+};
+
+$(document).ready(function () {
+    // display or hide sidebar
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
 
+    // display or hide category of menu items
     $('input[type="checkbox"]').on('click', function () {
         const categoryName = $(this).next().text();
         if ($(this).prop("checked") === true) {
@@ -17,13 +41,21 @@ $(document).ready(function () {
         }
     });
 
+    // search and exclude function
+    let inKey = undefined,
+        exKey = undefined;
+
     $("#search-input").on("keyup", function () {
-        const keyword = $(this).val().toLowerCase();
-        $(".slider-menu .dish-info-wrap").filter(function () {
-            $(this).toggle(
-                $(this).find('h6').text().toLowerCase().indexOf(keyword) > -1
-                || $(this).find('ul li p').text().toLowerCase().indexOf(keyword) > -1
-            );
-        });
+        inKey = $(this).val().toLowerCase(); // TODO regex match for alpha characters
+        if (!inKey || inKey.length < 2)
+            inKey = undefined;
+        hideOrShowMenuItem(inKey, exKey);
+    });
+
+    $("#exclude-input").on("keyup", function () {
+        exKey = $(this).val().toLowerCase();
+        if (!exKey || exKey.length < 2)
+            exKey = undefined;
+        hideOrShowMenuItem(inKey, exKey);
     });
 });
