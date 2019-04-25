87155b4e74d81798fa556e091653ef007b89c09d
Jason <duan_jason@outlook.com>
2019-04-25 15:29:20 -0400

    add navi on main pages

--- routes/order.js
+++ routes/order.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const utils = require('./utils');
 const asyncWrapper = utils.asyncWrapper;
 const orderModel = require('../data/order');
+const menuModel = require('../data/menu');
 
 router.get('/order', asyncWrapper(async (req, res, next) => {
     utils.debugPrint('Order Get:', req.session.user);
@@ -16,9 +17,18 @@ router.get('/order', asyncWrapper(async (req, res, next) => {
         console.error(err);
     });
 
+        let food_list = allOrders[i].foodList;
+        for(const k in food_list) {
+            const res = await menuModel.findMenuItemByID(k);
+            food_list[res.food_name] = food_list[k];
+            delete food_list[k];
+        }
+        allOrders[i].foodList = food_list;
+    }
+
     res.render('order', {
         title: 'Order',
-        orders: allOrder
+        orders: allOrders
     });
 }));
 
